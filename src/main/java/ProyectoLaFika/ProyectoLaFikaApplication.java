60fb5a7d2099c36e1e9289c7ff35e218a540a400
lawrence2807 <lawrencect28@gmail.com>
2023-04-01 18:38:28 -0600

    Modificación menú principal

---
+++ src/main/java/ProyectoLaFika/ProyectoLaFikaApplication.java
@@ -0,0 +1,13 @@
+package ProyectoLaFika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLaFikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLaFikaApplication.class, args);
+	}
+
+}
